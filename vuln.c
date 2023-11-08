020f5b35bc6c02f9cbaacff5a98527536b87e6eb
emilyd11 <emily@daviesland.net>
2023-11-07 21:11:33 -0500

    comments :wq
    i

--- vuln.c
+++ vuln.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv){
     /* initializing 256-byte buffer */
     char buf[256];
     printf("What's your name, kind stranger?\n");
+    /* writes unsanitized user input to buffer */
+    /* note - no length check! */
     scanf("%s", buf);
     printf("Hello, %s\n", buf);
     return 0;
